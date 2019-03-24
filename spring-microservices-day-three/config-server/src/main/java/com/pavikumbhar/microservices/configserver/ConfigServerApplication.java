9bcbd16187b00dd3687072cd158dd7af31bc6e9f
=Pavi Kumbhar <=pavikumbhar007@gmail.com>
2019-03-24 18:57:06 +0530

    commit

---
+++ spring-microservices-day-three/config-server/src/main/java/com/pavikumbhar/microservices/configserver/ConfigServerApplication.java
@@ -0,0 +1,27 @@
+
+package com.pavikumbhar.microservices.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+/**
+ * The Main Spring Boot Application class.<br>
+ * <br>
+ * 
+ * The {@link EnableConfigServer} annotation defines that this application will
+ * serve as the REST based API for providing external configuration. <br>
+ * <br>
+ * 
+ * The external repository from where the configuration will be picked up is
+ * defined in the {@linkplain application.yml} or {@linkplain application.properties} file.
+ * 
+ * @author pavikumbhar
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
